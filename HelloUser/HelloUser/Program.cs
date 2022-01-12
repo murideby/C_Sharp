967e16a1a96497464623518f35fc175c2eb042f0
Débora Murielle <debora.murielle@gmail.com>
2022-01-11 22:19:33 -0300

    Capturar entrada do usuário

---
+++ HelloUser/HelloUser/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloUser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "";
+            Console.WriteLine("Qual o seu nome ?");
+            nome = Console.ReadLine();
+            Console.WriteLine("Seu nome é:");
+            Console.WriteLine(nome);
+
+            Console.ReadLine();
+        }
+    }
+}
