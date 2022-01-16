bbd2880e9fe66cf1e3f3c430911051ee28f46f4c
Débora Murielle <debora.murielle@gmail.com>
2022-01-16 20:45:49 -0300

    Operadores Aritméticos - parte 2

--- Operadores_aritmeticos/Operadores_aritmeticos/Program.cs
+++ Operadores_aritmeticos/Operadores_aritmeticos/Program.cs
@@ -11,15 +11,18 @@ namespace Operadores_aritmeticos
         static void Main(string[] args)
         {
             int numeroSoma = 20 + 20;
-            int numeroSubtracao = 80 - 40;
+            int numeroSubtracao = 80 - 400;
             int numeroMultiplicacao = 10 * 90;
             int numeroDivisao = 6 / 2;
-
+            float numeroDecimal = 5.0f / 2;
+            int numeroSomaMult = (2 + 2) * 10;
 
             Console.WriteLine(numeroSoma);
             Console.WriteLine(numeroSubtracao); 
             Console.WriteLine(numeroMultiplicacao);
             Console.WriteLine(numeroDivisao);
+            Console.WriteLine(numeroDecimal);
+            Console.WriteLine(numeroSomaMult);
 
             Console.ReadLine();
         }
