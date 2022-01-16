cad4bb35ab32383fea5afa950579a855959b253d
Débora Murielle <debora.murielle@gmail.com>
2022-01-16 20:32:40 -0300

    Operadores Aritméticos

---
+++ Operadores_aritmeticos/Operadores_aritmeticos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operadores_aritmeticos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroSoma = 20 + 20;
+            int numeroSubtracao = 80 - 40;
+            int numeroMultiplicacao = 10 * 90;
+            int numeroDivisao = 6 / 2;
+
+
+            Console.WriteLine(numeroSoma);
+            Console.WriteLine(numeroSubtracao); 
+            Console.WriteLine(numeroMultiplicacao);
+            Console.WriteLine(numeroDivisao);
+
+            Console.ReadLine();
+        }
+    }
+}
