0fcd0f0130b68bf35b96a3357e5fe0a7f341520c
Débora Murielle <debora.murielle@gmail.com>
2022-01-20 22:46:47 -0300

    Exercicio3 - Condicionais

---
+++ Condicionais_Exercicio3/Condicionais_Exercicio3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Condicionais_Exercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int urgencia;
+
+            Console.WriteLine("Descreva sua solicitação!");
+            Console.ReadLine();
+            Console.WriteLine("Em uma escala de 0 a 10 informe o nível de urgência!");
+            urgencia = int.Parse(Console.ReadLine());
+
+            if (urgencia > 0 & urgencia <= 3)
+            {
+                Console.WriteLine("Sua urgência foi recebida e é de nível: BAIXO");
+            }
+            else if (urgencia > 3 & urgencia <= 6)
+            {
+                Console.WriteLine("Sua urgência foi recebida e é de nível: MÉDIO");
+            }
+            else if (urgencia > 6 & urgencia <=9)
+            {
+                Console.WriteLine("Sua urgência foi recebida e é de nível: ALTO");
+            }
+            else
+            {
+                Console.WriteLine("Sua urgência foi recebida e é de nível: GRAVE");
+            }
+            Console.ReadLine();
+        }
+    }
+}
